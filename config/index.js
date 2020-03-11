36b9f0c008a50c0336249249130fe7873bf076e1
Y2Nk4 <1147678303@qq.com>
2020-03-11 16:27:27 -0400

    update

--- config/index.js
+++ config/index.js
@@ -19,7 +19,11 @@ module.exports = {
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
+    bundleAnalyzerReport: process.env.npm_config_report,
+    transpileDependencies: [
+        'vue-echarts',
+        'resize-detector'
+    ]
   },
   dev: {
     env: require('./dev.env'),
